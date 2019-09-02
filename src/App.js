64399cd340cd37fc1a77a8f8e828ea9914a8b2b9
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-09-02 13:25:42 -0700

    Setting up App with state.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+
+import Todo from './components/TodoComponents/Todo.js'
+import TodoList from './components/TodoComponents/TodoList.js'
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      toDo: Todo
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -8,6 +18,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList {...this.state} />
       </div>
     );
   }
